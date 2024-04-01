959710258aad66eb199f48427cb4e47777db4975
ndeno <ndeno@users.noreply.github.com>
2024-04-01 09:04:03 +0100

    base again

--- apps/login/vite.config.ts
+++ apps/login/vite.config.ts
@@ -2,11 +2,13 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
+const DEV_DOMAIN = process.env.NDENO_DOMAIN || "";
+
 export default defineConfig(({ _command, mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    base: `https://dev.${JSON.stringify(env.NDENO_DOMAIN)}/login`,
+    base: `https://dev.${DEV_DOMAIN}/login`,
     define: {
       global: {},
       "process.env.NDENO_DEV_PUBLIC_CLIENT_1_ID": JSON.stringify(
