855b04e1197f726c644e10e1502504692704d17a
ndeno <ndeno@users.noreply.github.com>
2024-10-06 21:58:29 +0100

    comments added

--- packages/ndeno-utils/src/string/reverse.ts
+++ packages/ndeno-utils/src/string/reverse.ts
@@ -6,6 +6,8 @@ export default function reverse(input: string) {
   let inputArr = input.split("");
 
   while (pointer1 < pointer2) {
+    // destructures and creates temp variables on rhs before initializing lhs
+    // lhs are simultaneously initialized, so no overwriting
     [inputArr[pointer2], inputArr[pointer1]] = [
       inputArr[pointer1],
       inputArr[pointer2],
