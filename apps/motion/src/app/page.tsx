56332afcdc088246c15451ec254695e06d07dfef
ndeno <ndeno@users.noreply.github.com>
2024-10-26 17:57:29 +0100

    update routes

--- apps/motion/src/app/page.tsx
+++ apps/motion/src/app/page.tsx
@@ -1,35 +1,36 @@
 "use client";
 import React from "react";
-import SectionOne from "./components/SectionOne";
-import SectionTwo from "./components/SectionTwo";
-import { useScroll } from "framer-motion";
-import Lenis from "lenis";
 
 export default function Home() {
-  const container = React.useRef<HTMLElement | null>(null);
-
-  const { scrollYProgress } = useScroll({
-    target: container,
-
-    offset: ["start start", "end end"],
-  });
-
-  React.useEffect(() => {
-    const lenis = new Lenis();
-
-    function raf(time: number) {
-      lenis.raf(time);
-
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-  }, []);
-
   return (
-    <main ref={container} className="relative h-[200vh]">
-      <SectionOne scrollYProgress={scrollYProgress} />
-      <SectionTwo scrollYProgress={scrollYProgress} />
-    </main>
+    <div className="bg-[#0a0a0a]">
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+          <div className="text-center">
+            <h1 className="text-balance text-5xl font-semibold tracking-tight text-white sm:text-7xl">
+              ndeno Motion
+            </h1>
+            <p className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a
+                href="#"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Get started
+              </a>
+              <a
+                href="#"
+                className="text-sm font-semibold leading-6 text-white"
+              >
+                Learn more <span aria-hidden="true">→</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
