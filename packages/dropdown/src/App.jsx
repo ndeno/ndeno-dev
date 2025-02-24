f148d671c4aeee9d6db09314f62ad89337552e47
ndeno <ndeno@users.noreply.github.com>
2025-02-24 23:19:36 +0000

    add debounce

--- packages/dropdown/src/App.jsx
+++ packages/dropdown/src/App.jsx
@@ -1,19 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
+const debounce = (func, time) => {
+  let timer;
 
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => func(...args), time);
+  };
+};
 
 function App() {
-  const [query, setQuery] = useState("");
+  const [search, setSearch] = useState("");
+  const [debouncedSearch, setDebouncedSearch] = useState("");
+
+  useEffect(() => {
+    const handleUpdateSearch = debounce(setDebouncedSearch, 1500);
+
+    handleUpdateSearch(() => search);
+  }, [search]);
+
   return (
     <main>
       <h1>Dropdown</h1>
       <div className="dropdown-container">
-        <input
-          type="text"
-          value={query}
-          onChange={(e) => setQuery(() => e.target.value)}
-        />
+        <div className="dropdown-input">
+          <div>
+            <input
+              type="text"
+              value={search}
+              onChange={(e) => setSearch(() => e.target.value)}
+            />
+          </div>
+        </div>
+        {debouncedSearch}
       </div>
     </main>
   );
